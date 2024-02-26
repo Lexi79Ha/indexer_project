1a888d42bed3a52a8f7fa8525e04275b139f64f1
Lexi79Ha <139013867+Lexi79Ha@users.noreply.github.com>
2024-02-26 14:57:48 -0600

    Update and rename t.py to test.py

--- indexer/test.py
+++ indexer/test.py
@@ -2,18 +2,18 @@ import unittest
 import json
 import os
 import logging
-
+#Test to validate the files pulled by response command in Fetch.py
 class TestJSONValidation(unittest.TestCase):
-    def setUp(self):
+    def setUp(self): #pull file for testing
         # Replace with the actual folder path
         self.folder_path = "C:/Users/lmhmo/indexer/blocks"
 
-    def test_valid_json_files_exist(self):
+    def test_valid_json_files_exist(self): #Validate if the file is a json file
         # Get a list of all files in the directory
         all_files = os.listdir(self.folder_path)
         self.assertTrue(all_files, "No files found in the specified directory.")
 
-    def test_valid_json_format(self):
+    def test_valid_json_format(self): # Validate json file's formatting
         for filename in os.listdir(self.folder_path):
             full_path = os.path.join(self.folder_path, filename)
             with self.subTest(filename=filename):
@@ -25,12 +25,12 @@ class TestJSONValidation(unittest.TestCase):
                 except (ValueError, FileNotFoundError):
                     self.fail(f"{filename} is not a valid JSON file.")
 
-    def test_json_file_extension(self):
+    def test_json_file_extension(self): # Makes sure file ends in .json
         for filename in os.listdir(self.folder_path):
             with self.subTest(filename=filename):
                 self.assertTrue(filename.lower().endswith(".json"))
 
-    def test_height_positive_integer(self):
+    def test_height_positive_integer(self): # Height must be positive number for block chain
         for filename in os.listdir(self.folder_path):
             height = os.path.splitext(filename)[0]
             with self.subTest(filename=filename):
